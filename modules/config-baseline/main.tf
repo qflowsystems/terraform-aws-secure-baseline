7b69b4a82c27e62acde3c0e51cf4d839fb4d932e
Matt Martinez <matthewmartinez1003@gmail.com>
2023-12-12 11:26:29 -0600

    enable config thing

--- modules/config-baseline/main.tf
+++ modules/config-baseline/main.tf
@@ -37,32 +37,32 @@ data "aws_iam_policy_document" "config-sns-policy" {
   }
 }
 
-#resource "aws_config_configuration_recorder" "recorder" {
-#  name     = var.recorder_name
-#  role_arn = var.iam_role_arn
-#
-#  recording_group {
-#    all_supported                 = true
-#    include_global_resource_types = var.include_global_resource_types
-#  }
-#}
-#
-#resource "aws_config_delivery_channel" "bucket" {
-#  name           = var.delivery_channel_name
-#  s3_bucket_name = var.s3_bucket_name
-#  s3_key_prefix  = var.s3_key_prefix
-#  sns_topic_arn  = aws_sns_topic.config.arn
-#
-#  snapshot_delivery_properties {
-#    delivery_frequency = var.delivery_frequency
-#  }
-#
-#  depends_on = [aws_config_configuration_recorder.recorder]
-#}
-#
-#resource "aws_config_configuration_recorder_status" "recorder" {
-#  name       = aws_config_configuration_recorder.recorder.id
-#  is_enabled = true
-#
-#  depends_on = [aws_config_delivery_channel.bucket]
-#}
+resource "aws_config_configuration_recorder" "recorder" {
+  name     = var.recorder_name
+  role_arn = var.iam_role_arn
+
+  recording_group {
+    all_supported                 = true
+    include_global_resource_types = var.include_global_resource_types
+  }
+}
+
+resource "aws_config_delivery_channel" "bucket" {
+  name           = var.delivery_channel_name
+  s3_bucket_name = var.s3_bucket_name
+  s3_key_prefix  = var.s3_key_prefix
+  sns_topic_arn  = aws_sns_topic.config.arn
+
+  snapshot_delivery_properties {
+    delivery_frequency = var.delivery_frequency
+  }
+
+  depends_on = [aws_config_configuration_recorder.recorder]
+}
+
+resource "aws_config_configuration_recorder_status" "recorder" {
+  name       = aws_config_configuration_recorder.recorder.id
+  is_enabled = true
+
+  depends_on = [aws_config_delivery_channel.bucket]
+}
